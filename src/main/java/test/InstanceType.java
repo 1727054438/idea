6a7aaf9be7c74563eb27b481a42430833f6c0965
zhangsen <1727054438@qq.com>
2023-06-20 20:27:13 +0800

    修改

--- src/main/java/test/InstanceType.java
+++ src/main/java/test/InstanceType.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 //实例类型
 public class InstanceType {
     public int id;//实例类型id
-    public double capacity;//计算能力
+    public double storage;//存储能力
     public ArrayList<Price> prices;//数组大小为c+e 表示该实例类型在每个云中的价格 如果不在该云中则用无穷来表示
     public ArrayList<Integer> quantity;//数组大小为c+e  表示该实例类型在每个云的数量 一般为无穷
  }
