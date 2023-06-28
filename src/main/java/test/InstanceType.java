0a2f50d5235a006371899c2d838efe95b664259a
zhangsen <1727054438@qq.com>
2023-06-28 15:27:48 +0800

    修改

--- src/main/java/test/InstanceType.java
+++ src/main/java/test/InstanceType.java
@@ -1,5 +1,9 @@
 package test;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
@@ -12,8 +16,12 @@ import java.util.ArrayList;
  */
 //通用型------边缘实例类型      xlarge---一个边缘云  small--一个边缘云
 //实例类型
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class InstanceType {
     public int id;//实例类型id
+    public String name;//类型名
     public double storage;//存储能力
     public ArrayList<Price> prices;//数组大小为c+e 表示该实例类型在每个云中的价格 如果不在该云中则用无穷来表示
     public ArrayList<Integer> quantity;//数组大小为c+e  表示该实例类型在每个云的数量 一般为无穷
--- src/main/java/test/Price.java
+++ src/main/java/test/Price.java
@@ -1,5 +1,9 @@
 package test;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * 功能描述:
  *
@@ -8,6 +12,9 @@ package test;
  * @date 2023/5/24 15:11
  */
 //每个云或者边缘的价格
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Price {
     public double storagePrice;//存储价格
     public double bangWidthPrice;//带宽价格
